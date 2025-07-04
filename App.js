52acb079973647837c08672e04d43c8e8b1d0848
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-04 11:35:01 +0600

    native wind setup try

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import "./global.css"
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>RN Developer Ahasan Habib !</Text>
+  
+      <Text className="font-bold bg-green-400 text-purple-800">RN Developer polash !</Text>
+      <Text className="underline font-semibold">It's my first expo project</Text>
       <StatusBar style="auto" />
     </View>
   );
--- __all_command.js
+++ __all_command.js
@@ -1,7 +1,9 @@
 /***
  * 
- * npx create-expo-app@latest --template blank ./
+ * npx create-expo-app@latest --template blank ./ 
  * npx expo start
  * ctrl + c  press to exit the server
+ * npx expo start --clear
+ * npx rn-new@latest --nativewind
  * 
  */
